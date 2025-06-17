14b7cce4d740fc96546a72e25b1c433ef465a2d9
L-E-W-1-5 <lewiswootton88@hotmail.com>
2025-06-17 07:47:50 +0100

    added basic post to return question

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
-import express from 'express';
+const express = require('express');
 
 const app = express();
 
 app.get('/', (req, res) => {
     res.send('this is the server');
 });
+
+app.post('/', (req, res) => {
+    console.log(req.body);
+    res.send(req.body.question);
+})
 
 app.listen(3000, () => {
     console.log('server is listening on http://localhost:3000');
