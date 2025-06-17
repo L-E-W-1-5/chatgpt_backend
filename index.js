38f563dd6dabc3a93fbcc11366782d0796f053ab
L-E-W-1-5 <lewiswootton88@hotmail.com>
2025-06-17 18:59:36 +0100

    .env file added

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
-//const express = require('express');
+
 import express from "express";
 // import morgan from "morgan";
 import cors from 'cors'
+
+// require('dotenv').config();
+import 'dotenv/config';
+
+import OpenAI from "openai";
+
+const client = new OpenAI({apiKey: 'My API Key'});
 
 const app = express();
 
@@ -9,7 +16,7 @@ app.use(cors());
 
 app.use(express.json());
 
-const url = "https://chatgpt-backend-6uyd.onrender.com";
+
 
 app.get('/', (req, res) => {
     res.send('this is the server');
@@ -17,12 +24,41 @@ app.get('/', (req, res) => {
 
 app.post('/', (req, res) => {
     console.log(req.body);
-    res.json(req.body.question);
+
+    const question = req.body.question.trim();
+
+    var cleanedText = question.replace(/\s+/g, '');
+
+    const charCount = cleanedText.length;
+
+    console.log(charCount, req.body.question);
+
+    res.json({
+        "count": charCount, 
+        "question": question
+    });
+
+})
+
+
+app.post('api', async(req, res) => {
+
+    const question = req.body.question.trim();
+
+    console.log(question);
+
+    const response = await client.responses.create({
+        model: "gpt-4.1-nano",
+        input: question
+    });
+
+    console.log(response);
+
 })
 
 
 
-app.listen(url, () => {
-    console.log('server is listening on http://localhost:3000');
+app.listen(3000, () => {
+    console.log('server is listening on PORT 3000');
 });
 
