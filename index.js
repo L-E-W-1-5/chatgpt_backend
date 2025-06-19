34e5b162e93afa214cd1143470f496a6fbab28cc
L-E-W-1-5 <lewiswootton88@hotmail.com>
2025-06-19 10:32:03 +0100

    need to fix the api call to include the previous id

--- index.js
+++ index.js
@@ -7,12 +7,15 @@ import 'dotenv/config';
 import OpenAI from "openai";
 import {env} from 'node:process';
 
-const client = new OpenAI({apiKey: 'My API Key'});
+const email = env.EMAIL;
+const password = env.EMAIL_PASS;
+const api_key = env.GPT_KEY;
+
+const client = new OpenAI({apiKey: api_key});
 
 const app = express();
 
-const email = env.EMAIL;
-const password = env.EMAIL_PASS;
+
 
 app.use(cors());
 
@@ -31,17 +34,27 @@ app.get('/', (req, res) => {
 app.post('/', (req, res) => {           
     console.log(req.body);
 
-    const question = req.body.question.trim();
+    //const question = req.body.question.trim();
 
-    var cleanedText = question.replace(/\s+/g, '');
+    //var cleanedText = question.replace(/\s+/g, '');
 
-    const charCount = cleanedText.length;
+    //const charCount = cleanedText.length;
 
-    console.log(charCount, req.body.question);
+    //console.log(charCount, req.body.question);
+
+    const data = req.body.content[0].question;
+
+    console.log(req.body.content[0])
 
     res.json({
-        "count": charCount, 
-        "question": question
+
+         success: true,
+        payload: data,
+        response_id: data
+      
+        // success: charCount,
+        // payload: question,
+        // response_id: "55aa"
     });
 
 });
@@ -54,11 +67,11 @@ app.post('/email', (req, res) => {
 
     const subject = req.body.subject;
     const recipient = req.body.recipient;
-    const content = req.body.content;
+    const text = req.body.text;
 
-    console.log(subject, recipient, content);
+    console.log(subject, recipient, text);
 
-    if(subject && recipient && content){
+    if(subject && recipient && text){
 
         const transporter = nodemailer.createTransport({
             service: 'gmail',
@@ -73,7 +86,7 @@ app.post('/email', (req, res) => {
             from: `email from`,
             to: recipient,
             subject: subject,
-            text: content
+            text: text
 
         }, function(error, info){
 
@@ -107,18 +120,55 @@ app.post('/email', (req, res) => {
 
 
 // API Endpoint 
-app.post('api', async(req, res) => {
+app.post('/api', async(req, res) => {
 
     const question = req.body.question.trim();
+
+    console.log(req.body);
+
+    // var previous_id;
+
+    // if(req.body.previous_id != null){
+
+    //     previous_id = req.body.previous_id;
+
+    // }else {
+
+    //     previous_id = "";
+       
+    // }
 
     console.log(question);
 
     const response = await client.responses.create({
         model: "gpt-4.1-nano",
-        input: question
+        input: question,
+        max_output_tokens: 100,
+        // previous_response_id: previous_id
     });
 
     console.log(response);
+    console.log(response.output_text);
+    console.log(response.id)
+
+    if(response.output_text){
+
+        res.json({
+            success: true,
+            payload: response.output_text,
+            response_id: response.id
+        })
+
+    }else {
+
+        res.json({
+            success: false,
+            payload: "bad request",
+            response_id: ""
+        })
+    }
+
+
 
 })
 
