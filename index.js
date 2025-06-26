4cc4f18053053a23218b6b18986325ae97220980
L-E-W-1-5 <lewiswootton88@hotmail.com>
2025-06-26 10:44:57 +0100

    increase max_tokens

--- index.js
+++ index.js
@@ -159,7 +159,7 @@ app.post('/api', async(req, res) => {
     const response = await client.responses.create({
         model: "gpt-4.1-nano",
         input: question,
-        max_output_tokens: 500,
+        max_output_tokens: 800,
         previous_response_id: previous_id
     });
 
